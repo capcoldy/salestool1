d596f06807e7d189a84868399dfcbbd2120a71cb
sfdc cloud <sfdccloud@yahoo.com>
2018-08-06 12:43:12 +0530

    intial message java

---
+++ eclipse-workspace/testdata/src/testdata/testgit.java
@@ -0,0 +1,10 @@
+package testdata;
+
+public class testgit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("The total pay is "); //today data
+	}
+
+}
